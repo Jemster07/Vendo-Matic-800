41472fc74df5f7448f36024608123b0b423d9aa7
Dan Swiderski <nutmegpunk77@gmail.com>
2023-06-09 10:03:08 -0400

    capstone V1

---
+++ Capstone/Classes/FeedMoney.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Capstone.Classes
+{
+    public class FeedMoney : PurchaseMenu
+
+    { public FeedMoney(decimal balance) : base(balance)
+        {
+
+        }
+        public void CallFeedMoney()
+        {
+
+
+            Console.Write("Please Insert Money ($1,$5,$10,$20): ");
+            string input = Console.ReadLine();
+            while (input != "1" && input != "5" && input != "10" && input != "20")
+            {
+                Console.WriteLine("YOU EEEDIOT!!! Try Again");
+                Console.Write("Please Insert Money ($1,$5,$10,$20): ");
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"${input}");
+        }
+
+
+
+
+
+    }
+}
---
+++ Capstone/Classes/PurchaseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class PurchaseMenu
+
+    {
+        decimal Balance { get; set; } = 0.00M;
+        public PurchaseMenu(decimal balance)
+        {
+            Balance = balance;
+        }
+        public void CallPurchaseMenu()
+        {
+            
+
+            Console.WriteLine("What Would You Like to Do?:");
+            Console.WriteLine();
+            Console.WriteLine("(1) Feed Money");
+            Console.WriteLine("(2) Select Product");
+            Console.WriteLine("(3) Finish Transaction");
+            string input = Console.ReadLine();
+            string lowerInput = input.ToLower();
+            while (lowerInput != "1" && lowerInput != "(1)" && lowerInput != "feed money" 
+                && lowerInput != "2" && lowerInput != "(2)" && lowerInput != "select product"
+                && lowerInput != "3" && lowerInput != "(3)" && lowerInput != "finish transaction")
+            {
+                Console.WriteLine("WRONG!!!! Try Again Dummy...");
+            }
+
+            
+        }
+    }
+}
